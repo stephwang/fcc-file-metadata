f4061c7c1fb27c37b8443d3ce7c7f24b7ece3c07
Steph Wang <sw.steph@gmail.com>
2016-08-24 04:43:38 +0000

    it works!

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+var multer = require('multer');
+var upload = multer();
+
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.post('/get-file-size', upload.single('upload'), function(req,res){
+    res.send(JSON.stringify({size: req.file.size}));
+});
 
 app.listen(process.env.PORT || 8080);
